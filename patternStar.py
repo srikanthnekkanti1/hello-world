9bae20eced09ee56636db9dc4a9c0af0bc6f6fe1
Srikanth Nekkanti <srikanth.nekkanti1@gmail.com>
2021-08-22 19:57:02 -0500

    Added a python file patternStar.py

---
+++ patternStar.py
@@ -0,0 +1,22 @@
+def prnt():
+
+	j = int(input("Enter a number of rows to print: "))
+
+	for x in range(1,j+1):
+		i = 1
+		print(" "*(j-x), end = " ")
+		while i<=x:
+			print("*", end = " ")
+			i += 1
+		print()
+
+	for x in range(1,j+1):
+		i = 1
+		print(" "*(j-x), end = " ")
+		while i<=x:
+			print("*"*(j-x), end = " ")
+			i += 1
+		print()
+
+if __name__ == '__main__':
+	prnt()
